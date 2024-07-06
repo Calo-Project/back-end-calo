7c3194e1d41bc18c25b9e7cddf08be11b533d27a
altamisatmaja <altamisfattah16@gmail.com>
2024-07-07 01:18:58 +0700

    fix: logout

--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('user/login', [AuthCustomerController::class, 'login']);
 Route::post('user/register', [AuthCustomerController::class, 'register']);
-Route::post('user/logout', [AuthCustomerController::class, 'logout']);
 
 Route::middleware(['auth:sanctum', 'pengguna'])->group(function (){
+    Route::post('user/logout', [AuthCustomerController::class, 'logout']);
     Route::get('user/logged/', [AuthCustomerController::class, 'logged']);
 });
